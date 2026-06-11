d1d33f74fabbacd27659c80986705c6d1815f4d9
Enzo Wacker Guinossi <enzowackerguinossi@gmail.com>
2026-06-10 21:07:31 -0300

    feat: Cria Frame, MenuBar e esqueleto dos itens do menu

--- src/main/java/com/faetec/projetofinal/Main.java
+++ src/main/java/com/faetec/projetofinal/Main.java
@@ -1,11 +1,43 @@
 package com.faetec.projetofinal;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        JFrame frame = new JFrame("Software Gerenciamento Restaurante");
+        JMenuBar menuBar = new JMenuBar();
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+        JMenu arquivo = new JMenu("Arquivo");
+            arquivo.add(new JMenuItem("Sair"));
+        menuBar.add(arquivo);
+
+        JMenu cadastros = new JMenu("Cadastros");
+            JMenuItem itemClientes = new JMenuItem("Clientes");
+            JMenuItem itemGarcons = new JMenuItem("Garçons");
+            JMenuItem itemMesas = new JMenuItem("Mesas");
+            JMenuItem itemPratos = new JMenuItem("Pratos");
+
+            cadastros.add(itemClientes);
+            cadastros.add(itemGarcons);
+            cadastros.add(itemMesas);
+            cadastros.add(itemPratos);
+        menuBar.add(cadastros);
+
+
+        JMenu pedido = new JMenu("Pedidos");
+            pedido.add(new JMenuItem("Efetuar Pedido"));
+        menuBar.add(pedido);
+
+        JMenu ajuda = new JMenu("Ajuda");
+            ajuda.add(new JMenuItem("Sobre..."));
+        menuBar.add(ajuda);
+
+        frame.setJMenuBar(menuBar);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setResizable(false);
+
+        frame.setVisible(true);
     }
 }
